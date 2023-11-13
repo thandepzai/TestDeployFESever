5d09f61f48478486c2b51e1c8513d69e25b740d4
thandepzai <cuthan2k1@gmail.com>
2023-11-13 21:41:33 +0700

    add some

--- src/@Shop/pages/ProductList/SubmenuCategory.tsx
+++ src/@Shop/pages/ProductList/SubmenuCategory.tsx
@@ -8,8 +8,10 @@ const SubmenuCategory = ({ currentPath }: Props) => {
 	const source = currentPath.split('/')
 
 	if (source[1] === 'danh-muc')
-		return <div className="flex md:items-center flex-col text-xl font-semibold">{source[2]}</div>
-	return <div className="flex flex-col text-lg font-semibold">Tìm kiếm: {source[2]}</div>
+		return (
+			<div className="flex md:items-center flex-col text-xl font-semibold">{decodeURIComponent(source[2])}</div>
+		)
+	return <div className="flex flex-col text-lg font-semibold">Tìm kiếm: {decodeURIComponent(source[2])}</div>
 }
 
 export default SubmenuCategory
